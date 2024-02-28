2da76ed25b5c3a4aae3604de867a8fb761e454de
mrfchang03 <mrfchang03@gmail.com>
2024-02-27 20:58:39 -0800

    project border

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,46 +1,47 @@
 $(document).ready(function() {
   var learnMoreButton = $('.learn-more-button');
-  var originalPosition = learnMoreButton.offset().top; // Get the original top offset of the button
+  var arrowContainer = $('.arrow-container');
+  var originalPosition = learnMoreButton.offset().top;
+  var scrolled = false; // Flag to track if the user has scrolled
 
   function recalculateOriginalPosition() {
     originalPosition = learnMoreButton.offset().top;
-    console.log("Recalculated Original Position:", originalPosition); // Debugging position recalculation
-}
-$(window).resize(recalculateOriginalPosition);
+    console.log("Recalculated Original Position:", originalPosition);
+  }
+
+  $(window).resize(recalculateOriginalPosition);
 
   learnMoreButton.click(function(e) {
-      e.preventDefault(); // Prevent the default anchor behavior
-
-      // Animate scrolling to the projects section
-      $('html, body').animate({
-          scrollTop: $('#projects-section').offset().top
-      }, 3000, function() {
-          // Callback after animation completes
-          learnMoreButton.hide(); // Hide the button after scrolling down
-
-      });
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $('#projects-section').offset().top
+    }, 3000, function() {
+      learnMoreButton.hide();
+      arrowContainer.hide();
+    });
   });
 
   $(window).scroll(function() {
-      var scrollPos = $(this).scrollTop(); // Get current scroll position
-
-      // Show the button if user scrolls back up past the original position, hide otherwise
-      if (scrollPos < originalPosition) {
-          learnMoreButton.fadeIn();
-      } else {
-          learnMoreButton.fadeOut();
-      }
+    if (!scrolled) {
+      learnMoreButton.hide();
+      arrowContainer.hide();
+      scrolled = true;
+    }
   });
+
   recalculateOriginalPosition();
 });
 
 // Listen for scroll events on the window
 window.addEventListener('scroll', function() {
-    // Check if the page has been scrolled down from the top
-    if (window.pageYOffset > 0) {
-      document.body.classList.add('gradient-invert');
-    } else {
-      document.body.classList.remove('gradient-invert');
-    }
-  });
-  
+  // Check if the page has been scrolled down from the top
+  if (window.pageYOffset > 0) {
+    document.body.classList.add('gradient-invert');
+    $('.learn-more-button').hide(); // Hide the learn more button when the user starts to scroll
+    $('.arrow-container').hide(); // Ensure the arrow is also hidden
+  } else {
+    document.body.classList.remove('gradient-invert');
+    $('.learn-more-button').show(); // Show the learn more button when scrolled to the top
+    $('.arrow-container').show(); // Show the arrow when scrolled to the top
+  }
+});
